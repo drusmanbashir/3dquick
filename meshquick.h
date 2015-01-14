d3a3de66d970faffa0ff354babe66459a7ebf1da
Usman Bashir <drusmanbashir@gmail.com>
2015-01-14 02:19:55 +0000

    .

--- meshquick.h
+++ meshquick.h
@@ -1,5 +1,32 @@
 #ifndef MESHQUICK
 #define MESHQUICK
 
+#include <QtQuick/QQuickItem>
+
+
+
+class MeshQuick : public QQuickItem {
+
+public:
+    MeshQuick();
+    ~MeshQuick();
+
+
+
+
+public slots:
+
+    void sync();
+    void cleanup();
+
+
+
+private:
+
+
+};
+
+
+
 #endif // MESHQUICK
 
